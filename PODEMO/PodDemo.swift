7c4fe9bb57455b64f03b59e3ee81c9d872fcd271
Prashant <prashant.aggarwal@berylsystems.com>
2020-11-03 21:20:13 +0530

    lib sample file added

---
+++ PODEMO/PodDemo.swift
@@ -0,0 +1,21 @@
+//
+//  PodDemo.swift
+//  PODEMO
+//
+//  Created by Prashant on 03/11/20.
+//
+
+import Foundation
+public final class PodDemo {
+
+    let name = "SwiftyLib"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+    
+}
