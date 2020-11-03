190127da837278e58bb02bf5a4e7e7776a3ddac2
Prashant <prashant.aggarwal@berylsystems.com>
2020-11-03 21:16:56 +0530

    Initial setup

---
+++ PODEMO/PODEMO.h
@@ -0,0 +1,18 @@
+//
+//  PODEMO.h
+//  PODEMO
+//
+//  Created by Prashant on 03/11/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PODEMO.
+FOUNDATION_EXPORT double PODEMOVersionNumber;
+
+//! Project version string for PODEMO.
+FOUNDATION_EXPORT const unsigned char PODEMOVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PODEMO/PublicHeader.h>
+
+
